364b3e6f6538cd10eb9fbeac62815789320b1d34
satishkarengula <satish.karengula@gmail.com>
2024-03-28 11:09:14 +0800

    corrected subnet name

--- hub_firewall.tf
+++ hub_firewall.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "hub_fw_subnet" {
-  name                 = "fw-hub-subnet"
+  name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = ["10.0.3.0/26"]
