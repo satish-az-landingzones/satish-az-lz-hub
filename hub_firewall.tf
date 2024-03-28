da9b57b83539366b5c6c273a03f4a97c3b1fe497
satishkarengula <satish.karengula@gmail.com>
2024-03-28 11:01:14 +0800

    corrected rg

--- hub_firewall.tf
+++ hub_firewall.tf
@@ -1,26 +1,21 @@
-resource "azurerm_resource_group" "hub_fw_rg" {
-  name     = "rg-fw-hub-dev-eastus-001"
-  location = var.default_location
-}
-
 resource "azurerm_subnet" "hub_fw_subnet" {
   name                 = "fw-hub-subnet"
-  resource_group_name  = azurerm_resource_group.hub_fw_rg.name
+  resource_group_name  = azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = ["10.0.3.0/26"]
 }
 
 resource "azurerm_public_ip" "hub_fw_pip" {
   name                = "pip_fw_hub_dev_eastus_001"
-  resource_group_name = azurerm_resource_group.hub_fw_rg.name
-  location            = azurerm_resource_group.hub_fw_rg.location
+  resource_group_name = azurerm_resource_group.hub.name
+  location            = azurerm_resource_group.hub.location
   allocation_method   = "Static"
 }
 
 resource "azurerm_firewall" "hub_fw" {
   name                = "fw_hub_dev_eastus_001"
-  location            = azurerm_resource_group.hub_fw_rg.location
-  resource_group_name = azurerm_resource_group.hub_fw_rg.name
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
   sku_name            = "AZFW_Hub"
   sku_tier            = "Standard"
 
