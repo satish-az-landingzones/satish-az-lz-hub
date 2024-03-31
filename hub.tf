22c4fa630f6683f483fdc53546c6565fa8209004
satishkarengula <satish.karengula@gmail.com>
2024-04-01 00:45:12 +0800

    added hub and spoke

--- hub.tf
+++ hub.tf
@@ -31,3 +31,119 @@ resource "azurerm_virtual_network" "hub" {
     environment = "Production"
   }
 }
+
+
+# ## deploy hub and spoke network topology
+
+# data "azurerm_subscription" "current" {
+# }
+
+
+# # register Microsoft.Network to the Management Group
+
+# resource "null_resource" "register_rp_to_mg" {
+#   provisioner "local-exec" {
+#     command = "az provider register --namespace Microsoft.Network -m ${azurerm_management_group.mg.name}"
+#   }
+#   depends_on = [azurerm_role_assignment.management_group_owner]
+# }
+
+# resource "time_sleep" "wait_5_seconds" {
+#   create_duration = "5s"
+#   depends_on      = [null_resource.register_rp_to_mg]
+# }
+
+# # Create a Virtual Network Manager instance
+
+# resource "azurerm_network_manager" "network_manager_instance" {
+#   name                = "network-manager"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   scope_accesses      = ["Connectivity"]
+#   description         = "example network manager"
+#   scope {
+#     management_group_ids = [azurerm_management_group.mg.id]
+#   }
+#   depends_on = [time_sleep.wait_5_seconds]
+# }
+
+# # Create a network group
+
+# resource "azurerm_network_manager_network_group" "network_group" {
+#   name               = "network-group"
+#   network_manager_id = azurerm_network_manager.network_manager_instance.id
+# }
+
+# # Add three virtual networks to a network group as dynamic members with Azure Policy
+
+# resource "random_pet" "network_group_policy_name" {
+#   prefix = "network-group-policy"
+# }
+
+# resource "azurerm_policy_definition" "network_group_policy" {
+#   name         = random_pet.network_group_policy_name.id
+#   policy_type  = "Custom"
+#   mode         = "Microsoft.Network.Data"
+#   display_name = "Policy Definition for Network Group"
+
+#   metadata = <<METADATA
+#     {
+#       "category": "Azure Virtual Network Manager"
+#     }
+#   METADATA
+
+#   policy_rule = <<POLICY_RULE
+#     {
+#       "if": {
+#         "allOf": [
+#           {
+#               "field": "type",
+#               "equals": "Microsoft.Network/virtualNetworks"
+#           },
+#           {
+#             "allOf": [
+#               {
+#               "field": "Name",
+#               "contains": "${random_pet.virtual_network_name.id}"
+#               }
+#             ]
+#           }
+#         ]
+#       },
+#       "then": {
+#         "effect": "addToNetworkGroup",
+#         "details": {
+#           "networkGroupId": "${azurerm_network_manager_network_group.network_group.id}"
+#         }
+#       }
+#     }
+#   POLICY_RULE
+# }
+
+# resource "azurerm_subscription_policy_assignment" "azure_policy_assignment" {
+#   name                 = "${random_pet.network_group_policy_name.id}-policy-assignment"
+#   policy_definition_id = azurerm_policy_definition.network_group_policy.id
+#   subscription_id      = data.azurerm_subscription.current.id
+# }
+
+# # Create a connectivity configuration
+
+# resource "azurerm_network_manager_connectivity_configuration" "connectivity_config" {
+#   name                  = "connectivity-config"
+#   network_manager_id    = azurerm_network_manager.network_manager_instance.id
+#   connectivity_topology = "Mesh"
+#   applies_to_group {
+#     group_connectivity = "None"
+#     network_group_id   = azurerm_network_manager_network_group.network_group.id
+#   }
+# }
+
+
+# # Commit deployment
+
+# resource "azurerm_network_manager_deployment" "commit_deployment" {
+#   network_manager_id = azurerm_network_manager.network_manager_instance.id
+#   location           = azurerm_resource_group.rg.location
+#   scope_access       = "Connectivity"
+#   configuration_ids  = [azurerm_network_manager_connectivity_configuration.connectivity_config.id]
+# }
--- hub_firewall.tf
+++ hub_firewall.tf
@@ -1,27 +1,27 @@
-resource "azurerm_subnet" "hub_fw_subnet" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.hub.name
-  virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes     = ["10.0.3.0/26"]
-}
+# resource "azurerm_subnet" "hub_fw_subnet" {
+#   name                 = "AzureFirewallSubnet"
+#   resource_group_name  = azurerm_resource_group.hub.name
+#   virtual_network_name = azurerm_virtual_network.hub.name
+#   address_prefixes     = ["10.0.3.0/26"]
+# }
 
-resource "azurerm_public_ip" "hub_fw_pip" {
-  name                = "pip_fw_hub_dev_eastus_001"
-  resource_group_name = azurerm_resource_group.hub.name
-  location            = azurerm_resource_group.hub.location
-  allocation_method   = "Static"
-}
+# resource "azurerm_public_ip" "hub_fw_pip" {
+#   name                = "pip_fw_hub_dev_eastus_001"
+#   resource_group_name = azurerm_resource_group.hub.name
+#   location            = azurerm_resource_group.hub.location
+#   allocation_method   = "Static"
+# }
 
-resource "azurerm_firewall" "hub_fw" {
-  name                = "fw_hub_dev_eastus_001"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
-  sku_name            = "AZFW_Hub"
-  sku_tier            = "Standard"
+# resource "azurerm_firewall" "hub_fw" {
+#   name                = "fw_hub_dev_eastus_001"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
+#   sku_name            = "AZFW_Hub"
+#   sku_tier            = "Standard"
 
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.hub_fw_subnet.id
-    public_ip_address_id = azurerm_public_ip.hub_fw_pip.id
-  }
-}
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = azurerm_subnet.hub_fw_subnet.id
+#     public_ip_address_id = azurerm_public_ip.hub_fw_pip.id
+#   }
+# }
