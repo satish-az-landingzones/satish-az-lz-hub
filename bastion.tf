90f1ce015613f1d9dcf95470bd2ba3e774688c58
satishkarengula <satish.karengula@gmail.com>
2024-04-04 01:13:40 +0800

    udpated bastion

--- bastion.tf
+++ bastion.tf
@@ -6,13 +6,6 @@ resource "azurerm_public_ip" "bastion" {
   sku                 = "Standard"
 }
 
-data "azurerm_subnet" "azure_bastion_subnet" {
-  depends_on           = [azurerm_virtual_network.hub]
-  name                 = "AzureBastionSubnet"
-  virtual_network_name = azurerm_virtual_network.hub.name
-  resource_group_name  = azurerm_resource_group.hub.name
-}
-
 resource "azurerm_bastion_host" "bastion" {
   name                = "hub_bastion"
   location            = azurerm_resource_group.hub.location
--- hub.tf
+++ hub.tf
@@ -9,7 +9,7 @@ resource "azurerm_network_security_group" "hub" {
   resource_group_name = azurerm_resource_group.hub.name
 }
 
-resource "azurerm_network_security_group" "sg_hub_bastion" {
+resource "azurerm_network_security_group" "bastion" {
   name                = "bastion-security-group"
   location            = azurerm_resource_group.hub.location
   resource_group_name = azurerm_resource_group.hub.name
@@ -35,8 +35,8 @@ resource "azurerm_virtual_network" "hub" {
 
   subnet {
     name           = "AzureBastionSubnet"
-    address_prefix = "10.0.5.0/26"
-    security_group = azurerm_network_security_group.sg_hub_bastion.id
+    address_prefix = "10.0.3.0/26"
+    security_group = azurerm_network_security_group.bastion.id
   }
 
   tags = {
@@ -44,3 +44,77 @@ resource "azurerm_virtual_network" "hub" {
   }
 }
 
+data "azurerm_subnet" "bastion_subnet" {
+  depends_on           = [azurerm_virtual_network.hub]
+  name                 = "AzureBastionSubnet"
+  virtual_network_name = azurerm_virtual_network.hub.name
+  resource_group_name  = azurerm_resource_group.hub.name
+}
+
+
+
+resource "azurerm_subnet_network_security_group_association" "subnet" {
+  subnet_id                 = data.azurerm_subnet.bastion_subnet.id
+  network_security_group_id = azurerm_network_security_group.bastion.id
+
+  depends_on = [azurerm_network_security_rule.allow_https_inbound,
+    azurerm_network_security_rule.allow_gateway_manager_inbound,
+    azurerm_network_security_rule.allow_sshrdp_outbound,
+  azurerm_network_security_rule.allow_azurecloud_outbound]
+}
+
+resource "azurerm_network_security_rule" "allow_https_inbound" {
+  name                        = "AllowHttpsInbound"
+  priority                    = 100
+  direction                   = "inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.bastion.name
+  network_security_group_name = azurerm_network_security_group.bastion.name
+}
+
+resource "azurerm_network_security_rule" "allow_gateway_manager_inbound" {
+  name                        = "AllowGatewayManagerInbound"
+  priority                    = 110
+  direction                   = "inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "GatewayManager"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.bastion.name
+  network_security_group_name = azurerm_network_security_group.bastion.name
+}
+
+resource "azurerm_network_security_rule" "allow_sshrdp_outbound" {
+  name                        = "AllowSshRdpOutbound"
+  priority                    = 100
+  direction                   = "outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["22", "3389"]
+  source_address_prefix       = "*"
+  destination_address_prefix  = "VirtualNetwork"
+  resource_group_name         = data.azurerm_resource_group.bastion.name
+  network_security_group_name = azurerm_network_security_group.bastion.name
+}
+
+resource "azurerm_network_security_rule" "allow_azurecloud_outbound" {
+  name                        = "AllowAzureCloudOutbound"
+  priority                    = 110
+  direction                   = "outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "AzureCloud"
+  resource_group_name         = data.azurerm_resource_group.bastion.name
+  network_security_group_name = azurerm_network_security_group.bastion.name
+}
