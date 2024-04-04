d7d7802290d54fb8432b0201177ba4152d4df9fc
satishkarengula <satish.karengula@gmail.com>
2024-04-04 08:25:57 +0800

    destroy hub

--- hub.tf
+++ hub.tf
@@ -1,120 +1,120 @@
-resource "azurerm_resource_group" "hub" {
-  name     = "hub-resources"
-  location = var.default_location
-}
+# resource "azurerm_resource_group" "hub" {
+#   name     = "hub-resources"
+#   location = var.default_location
+# }
 
-resource "azurerm_network_security_group" "hub" {
-  name                = "hub-security-group"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
-}
+# resource "azurerm_network_security_group" "hub" {
+#   name                = "hub-security-group"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
+# }
 
-resource "azurerm_network_security_group" "bastion" {
-  name                = "bastion-security-group"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
-}
+# resource "azurerm_network_security_group" "bastion" {
+#   name                = "bastion-security-group"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
+# }
 
-resource "azurerm_virtual_network" "hub" {
-  name                = "hub-network"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+# resource "azurerm_virtual_network" "hub" {
+#   name                = "hub-network"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
+#   address_space       = ["10.0.0.0/16"]
+#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
+#   subnet {
+#     name           = "subnet1"
+#     address_prefix = "10.0.1.0/24"
+#   }
 
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.hub.id
-  }
+#   subnet {
+#     name           = "subnet2"
+#     address_prefix = "10.0.2.0/24"
+#     security_group = azurerm_network_security_group.hub.id
+#   }
 
-  subnet {
-    name           = "AzureBastionSubnet"
-    address_prefix = "10.0.3.0/26"
-    security_group = azurerm_network_security_group.bastion.id
-  }
+#   subnet {
+#     name           = "AzureBastionSubnet"
+#     address_prefix = "10.0.3.0/26"
+#     security_group = azurerm_network_security_group.bastion.id
+#   }
 
-  tags = {
-    environment = "Production"
-  }
-}
+#   tags = {
+#     environment = "Production"
+#   }
+# }
 
-data "azurerm_subnet" "bastion_subnet" {
-  depends_on           = [azurerm_virtual_network.hub]
-  name                 = "AzureBastionSubnet"
-  virtual_network_name = azurerm_virtual_network.hub.name
-  resource_group_name  = azurerm_resource_group.hub.name
-}
+# data "azurerm_subnet" "bastion_subnet" {
+#   depends_on           = [azurerm_virtual_network.hub]
+#   name                 = "AzureBastionSubnet"
+#   virtual_network_name = azurerm_virtual_network.hub.name
+#   resource_group_name  = azurerm_resource_group.hub.name
+# }
 
 
 
-resource "azurerm_subnet_network_security_group_association" "subnet" {
-  subnet_id                 = data.azurerm_subnet.bastion_subnet.id
-  network_security_group_id = azurerm_network_security_group.bastion.id
+# resource "azurerm_subnet_network_security_group_association" "subnet" {
+#   subnet_id                 = data.azurerm_subnet.bastion_subnet.id
+#   network_security_group_id = azurerm_network_security_group.bastion.id
 
-  depends_on = [azurerm_network_security_rule.allow_https_Inbound,
-    azurerm_network_security_rule.allow_gateway_manager_Inbound,
-    azurerm_network_security_rule.allow_sshrdp_Outbound,
-  azurerm_network_security_rule.allow_azurecloud_Outbound]
-}
+#   depends_on = [azurerm_network_security_rule.allow_https_Inbound,
+#     azurerm_network_security_rule.allow_gateway_manager_Inbound,
+#     azurerm_network_security_rule.allow_sshrdp_Outbound,
+#   azurerm_network_security_rule.allow_azurecloud_Outbound]
+# }
 
-resource "azurerm_network_security_rule" "allow_https_Inbound" {
-  name                        = "AllowHttpsInbound"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "Internet"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.hub.name
-  network_security_group_name = azurerm_network_security_group.bastion.name
-}
+# resource "azurerm_network_security_rule" "allow_https_Inbound" {
+#   name                        = "AllowHttpsInbound"
+#   priority                    = 100
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "443"
+#   source_address_prefix       = "Internet"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.hub.name
+#   network_security_group_name = azurerm_network_security_group.bastion.name
+# }
 
-resource "azurerm_network_security_rule" "allow_gateway_manager_Inbound" {
-  name                        = "AllowGatewayManagerInbound"
-  priority                    = 110
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "GatewayManager"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.hub.name
-  network_security_group_name = azurerm_network_security_group.bastion.name
-}
+# resource "azurerm_network_security_rule" "allow_gateway_manager_Inbound" {
+#   name                        = "AllowGatewayManagerInbound"
+#   priority                    = 110
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "443"
+#   source_address_prefix       = "GatewayManager"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.hub.name
+#   network_security_group_name = azurerm_network_security_group.bastion.name
+# }
 
-resource "azurerm_network_security_rule" "allow_sshrdp_Outbound" {
-  name                        = "AllowSshRdpOutbound"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_ranges     = ["22", "3389"]
-  source_address_prefix       = "*"
-  destination_address_prefix  = "VirtualNetwork"
-  resource_group_name         = azurerm_resource_group.hub.name
-  network_security_group_name = azurerm_network_security_group.bastion.name
-}
+# resource "azurerm_network_security_rule" "allow_sshrdp_Outbound" {
+#   name                        = "AllowSshRdpOutbound"
+#   priority                    = 100
+#   direction                   = "Outbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_ranges     = ["22", "3389"]
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "VirtualNetwork"
+#   resource_group_name         = azurerm_resource_group.hub.name
+#   network_security_group_name = azurerm_network_security_group.bastion.name
+# }
 
-resource "azurerm_network_security_rule" "allow_azurecloud_Outbound" {
-  name                        = "AllowAzureCloudOutbound"
-  priority                    = 110
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "AzureCloud"
-  resource_group_name         = azurerm_resource_group.hub.name
-  network_security_group_name = azurerm_network_security_group.bastion.name
-}
+# resource "azurerm_network_security_rule" "allow_azurecloud_Outbound" {
+#   name                        = "AllowAzureCloudOutbound"
+#   priority                    = 110
+#   direction                   = "Outbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "443"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "AzureCloud"
+#   resource_group_name         = azurerm_resource_group.hub.name
+#   network_security_group_name = azurerm_network_security_group.bastion.name
+# }
--- hub_spoke_vnet_peering.tf
+++ hub_spoke_vnet_peering.tf
@@ -1,20 +1,20 @@
-data "azurerm_virtual_network" "spoke" {
-  name                = "spoke-network"
-  resource_group_name = "spoke-resources"
-  provider            = azurerm.spoke
-}
+# data "azurerm_virtual_network" "spoke" {
+#   name                = "spoke-network"
+#   resource_group_name = "spoke-resources"
+#   provider            = azurerm.spoke
+# }
 
-resource "azurerm_virtual_network_peering" "hub_to_spoke" {
-  name                      = "hub_to_spoke"
-  resource_group_name       = azurerm_resource_group.hub.name
-  virtual_network_name      = azurerm_virtual_network.hub.name
-  remote_virtual_network_id = data.azurerm_virtual_network.spoke.id
-}
+# resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+#   name                      = "hub_to_spoke"
+#   resource_group_name       = azurerm_resource_group.hub.name
+#   virtual_network_name      = azurerm_virtual_network.hub.name
+#   remote_virtual_network_id = data.azurerm_virtual_network.spoke.id
+# }
 
-resource "azurerm_virtual_network_peering" "spoke_to_hub" {
-  name                      = "spoke_to_hub"
-  resource_group_name       = "spoke-resources"
-  virtual_network_name      = data.azurerm_virtual_network.spoke.name
-  remote_virtual_network_id = azurerm_virtual_network.hub.id
-  provider                  = azurerm.spoke
-}
+# resource "azurerm_virtual_network_peering" "spoke_to_hub" {
+#   name                      = "spoke_to_hub"
+#   resource_group_name       = "spoke-resources"
+#   virtual_network_name      = data.azurerm_virtual_network.spoke.name
+#   remote_virtual_network_id = azurerm_virtual_network.hub.id
+#   provider                  = azurerm.spoke
+# }
