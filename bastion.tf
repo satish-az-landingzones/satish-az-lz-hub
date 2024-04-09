2efa8451be2fc0b640cde0c8022795bd92ecc9c5
satishkarengula <satish.karengula@gmail.com>
2024-04-10 01:59:19 +0800

    spoke destroy

--- bastion.tf
+++ bastion.tf
@@ -1,19 +1,19 @@
-resource "azurerm_public_ip" "bastion" {
-  name                = "hub_bastion_pip"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+# resource "azurerm_public_ip" "bastion" {
+#   name                = "hub_bastion_pip"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
 
-resource "azurerm_bastion_host" "bastion" {
-  name                = "hub_bastion"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
+# resource "azurerm_bastion_host" "bastion" {
+#   name                = "hub_bastion"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
 
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.bastion_subnet.id
-    public_ip_address_id = azurerm_public_ip.bastion.id
-  }
-}
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = azurerm_subnet.bastion_subnet.id
+#     public_ip_address_id = azurerm_public_ip.bastion.id
+#   }
+# }
